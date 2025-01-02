0b356ea8284b3fda24e088ef0b82c59357040c98
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-01-03 00:18:59 +0100

    finishing touches to the category section

--- components/FirstGroupCard.jsx
+++ components/FirstGroupCard.jsx
@@ -1,45 +1,45 @@
 export default function FirstCard() {
     return (
         <div className="flex flex-col md:flex-row gap-3 items-center justify-between">
-            <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+            <div className="w-full p-6 border border-gray-300 rounded-sm">
                 <div className="flex flex-col gap-4">
                     <img src="../Cross.png" alt="A cross like object" width={40} />
-                    <h3>Design</h3>
+                    <h3 className="font-bold text-2xl">Design</h3>
                     <div className="flex gap-2">
-                        <p>235 jobs available</p>
+                        <p className="text-xl text-gray-400">235 jobs available</p>
                         <img src="../Icon.png" alt="right arrow" />
                     </div>
                 </div>
             </div>
 
-            <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+            <div className="w-full p-6 border border-gray-300 rounded-sm">
                 <div className="flex flex-col gap-4">
                     <img src="../Graph.png" alt="A graph like object" width={40} />
-                    <h3>Design</h3>
+                    <h3 className="font-bold text-2xl">Sales</h3>
                     <div className="flex gap-2">
-                        <p>235 jobs available</p>
+                        <p className="text-xl text-gray-400">756 jobs available</p>
                         <img src="../Icon.png" alt="right arrow" />
                     </div>
                 </div>
             </div>
 
-            <div className="w-full p-6 bg-blue-700 text-white border-2 border-gray-600 rounded-sm">
+            <div className="w-full p-6 bg-blue-700 text-white border border-gray-300 rounded-sm">
                 <div className="flex flex-col gap-4">
                     <img src="../Vector.png" alt="A vector like object" width={40} />
-                    <h3>Design</h3>
+                    <h3 className="font-bold text-2xl">Marketing</h3>
                     <div className="flex gap-2">
-                        <p>235 jobs available</p>
+                        <p className="text-xl text-white">140 jobs available</p>
                         <img src="../Icon.png" alt="right arrow" />
                     </div>
                 </div>
             </div>
             
-            <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+            <div className="w-full p-6 border border-gray-300 rounded-sm">
                 <div className="flex flex-col gap-4">
                     <img src="../Card.png" alt="A card like object" width={40} />
-                    <h3>Design</h3>
+                    <h3 className="font-bold text-2xl">Finance</h3>
                     <div className="flex gap-2">
-                        <p>235 jobs available</p>
+                        <p className="text-xl text-gray-400">325 jobs available</p>
                         <img src="../Icon.png" alt="right arrow" />
                     </div>
                 </div>
--- components/SecondGroupCard.jsx
+++ components/SecondGroupCard.jsx
@@ -2,45 +2,45 @@ export default function SecondCard() {
     return (
         <div className="mt-10">
             <div className="flex flex-col md:flex-row gap-3 items-center justify-between">
-                <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+                <div className="w-full p-6 border border-gray-300 rounded-sm">
                     <div className="flex flex-col gap-4">
                         <img src="../Tele.png" alt="A television like object" width={40} />
-                        <h3>Design</h3>
+                        <h3 className="font-bold text-2xl">Technology</h3>
                         <div className="flex gap-2">
-                            <p>235 jobs available</p>
+                            <p className="text-xl text-gray-400">436 jobs available</p>
                             <img src="../Icon.png" alt="right arrow" />
                         </div>
                     </div>
                 </div>
 
-                <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+                <div className="w-full p-6 border border-gray-300 rounded-sm">
                     <div className="flex flex-col gap-4">
                         <img src="../Angle.png" alt="A angle object" width={40} />
-                        <h3>Design</h3>
+                        <h3 className="font-bold text-2xl">Engineering</h3>
                         <div className="flex gap-2">
-                            <p>235 jobs available</p>
+                            <p className="text-xl text-gray-400">542 jobs available</p>
                             <img src="../Icon.png" alt="right arrow" />
                         </div>
                     </div>
                 </div>
 
-                <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+                <div className="w-full p-6 border border-gray-300 rounded-sm">
                     <div className="flex flex-col gap-4">
                         <img src="../Bag.png" alt="A bag like object" width={40} />
-                        <h3>Design</h3>
+                        <h3 className="font-bold text-2xl">Business</h3>
                         <div className="flex gap-2">
-                            <p>135 jobs available</p>
+                            <p className="text-xl text-gray-400">211 jobs available</p>
                             <img src="../Icon.png" alt="right arrow" />
                         </div>
                     </div>
                 </div>
                 
-                <div className="w-full p-6 border-2 border-gray-600 rounded-sm">
+                <div className="w-full p-6 border border-gray-300 rounded-sm">
                     <div className="flex flex-col gap-4">
                         <img src="../People.png" alt="A group" width={40} />
-                        <h3>Design</h3>
+                        <h3 className="font-bold text-2xl">Human Resource</h3>
                         <div className="flex gap-2">
-                            <p>260 jobs available</p>
+                            <p className="text-xl text-gray-400">346 jobs available</p>
                             <img src="../Icon.png" alt="right arrow" />
                         </div>
                     </div>
